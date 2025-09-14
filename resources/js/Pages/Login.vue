ede15bb011bb790376907b3399bc602b10bb9529
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2025-09-14 21:13:47 +0200

    modif

---
+++ resources/js/Pages/Login.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <h2 class="text-3xl font-bold mb-4">Connexion</h2>
+    <button >
+      Retour
+    </button>
+  </div>
+</template>
