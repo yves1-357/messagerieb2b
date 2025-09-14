c477489de8fafb18cf451f4c6535e1e8ca2e1eea
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2025-09-14 21:16:25 +0200

    Mise à jour des routes pour AuthPage

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/', function () {
+    return Inertia::render('Authpage');
+})->name('home');
+
 
 require __DIR__.'/auth.php';
