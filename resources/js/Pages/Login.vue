2789075a1dc2a0d103d3ceec671527dc5b751897
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2025-09-25 15:32:24 +0200

    login/register

--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -5,15 +5,15 @@
     <form>
       <div class="mb-4">
         <label class="block text-gray-400 mb-2">Email</label>
-        <input type="email" class="w-full px-4 py-2 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500" />
+        <input type="email" v-model="email" class="w-full px-4 py-2 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500" />
       </div>
 
       <div class="mb-6">
         <label class="block text-gray-400 mb-2">Mot de passe</label>
-        <input type="password" class="w-full px-4 py-2 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500" />
+        <input type="password" v-model="password" class="w-full px-4 py-2 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500" />
       </div>
 
-      <button type="button" class="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition">
+      <button type="button" @click="handleLogin" :disabled="chargement" class="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition">
         Se connecter
       </button>
     </form>
@@ -24,3 +24,80 @@
     </p>
   </div>
 </template>
+<script setup>
+import axios from 'axios';
+import {ref } from 'vue';
+import { router } from '@inertiajs/vue3';
+import Swal from 'sweetalert2';
+
+
+// on declare les proprites vue
+const email = ref ('');
+const password = ref('');
+const chargement = ref(false);
+
+async function handleLogin() {
+    console.log('handleLogin appelée');
+  if (chargement.value) return;
+   chargement.value = true;
+
+    if(!email.value) {
+        alert('L\'email est requis.');
+        chargement.value = false;
+        return;
+    }
+
+    if(!email.value.includes('@')){
+        alert('Veuillez entrez un email valide.');
+        chargement.value = false;
+        return;
+    }
+    if (password.value.length < 5) {
+        alert('Le mot de passe doit contenir au moins 5 caractères.')
+        chargement.value = false;
+        return;
+
+    }
+
+
+    try {
+        //envoie donnes backend
+        const response = await axios.post('/login', {
+            email:email.value,
+            password:password.value,
+        });
+        //si connexion reussie
+        console.log('connexion reussie', response.data);
+        Swal.fire({
+        title: 'Succès !',
+        text: 'Connexion réussie !',
+        icon: 'success',
+        confirmButtonText: 'OK'
+    }).then(() => {
+        router.visit('/chat');
+    });
+
+    }
+    catch (error) {
+    console.error('Erreur de connexion:', error.response?.data);
+
+    let errorMessage = "Une erreur est survenue lors de la connexion.";
+
+    if (error.response?.data?.message) {
+        errorMessage = error.response.data.message;
+    }
+
+    Swal.fire({
+        title: 'Erreur',
+        text: errorMessage,
+        icon: 'error',
+        confirmButtonText: 'OK'
+    });
+}
+    finally {
+        chargement.value = false;
+    }
+    }
+
+
+</script>
--- resources/js/Pages/Register.vue
+++ resources/js/Pages/Register.vue
@@ -2,28 +2,23 @@
   <div class="bg-gray-800 p-4 rounded-lg shadow-lg w-96">
     <h2 class="text-white text-xl font-bold mb-2 text-center">Inscription</h2>
 
-    <form>
-      <div class="mb-2">
-        <label class="block text-gray-400 mb-2">Pseudo</label>
-        <input type="text" class="w-full px-4 py-2 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500" />
-      </div>
-
-      <div class="mb-2">
+    <form @submit.prevent="handleRegister">
+           <div class="mb-2">
         <label class="block text-gray-400 mb-2">Nom</label>
-        <input type="text" class="w-full px-4 py-2 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500" />
+        <input type="text"  v-model="nom" required  class="w-full px-4 py-2 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500" />
       </div>
 
       <div class="mb-2">
         <label class="block text-gray-400 mb-2">Email</label>
-        <input type="email" class="w-full px-4 py-2 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500" />
+        <input type="email" v-model="email" required  class="w-full px-4 py-2 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500" />
       </div>
 
       <div class="mb-2">
         <label class="block text-gray-400 mb-2">Mot de passe</label>
-        <input type="password" class="w-full px-4 py-2 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500" />
+        <input type="password" v-model="password" required  class="w-full px-4 py-2 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500" />
       </div>
 
-      <button type="button" class="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition">
+      <button type="submit"  :disabled="chargement" class="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition">
         S’inscrire
       </button>
     </form>
@@ -34,3 +29,102 @@
     </p>
   </div>
 </template>
+<script setup>
+import {ref} from 'vue';
+import axios from 'axios';
+import { router } from '@inertiajs/vue3';
+import Swal from 'sweetalert2';
+
+
+
+// on declare
+const nom = ref('test');
+const email = ref('default@exemple.com');
+const password = ref('holaZola');
+const chargement = ref(false);
+const emit = defineEmits(['switchPage']);
+
+async function handleRegister(){
+
+    console.log('Debut handleRegister');
+    chargement.value = true;
+
+
+    if(!nom.value){
+        alert('le nom est requis.');
+        chargement.value = false;
+        return;
+    }
+    if(!email.value) {
+        alert('L\'email est requis.');
+        chargement.value = false;
+        return;
+    }
+
+    if(!email.value.includes('@')){
+        alert('Veuillez entrez un email valide.');
+        chargement.value = false;
+        return;
+    }
+    if (password.value.length < 5) {
+        alert('Le mot de passe doit contenir au moins 5 caractères.')
+        chargement.value = false;
+        return;
+
+    }
+
+ try {
+    const response = await axios.post('/register', {
+      name: nom.value,
+      email: email.value,
+      password: password.value,
+      password_confirmation: password.value
+    });
+
+    console.log('Réponse serveur:', response.data);
+
+    // Afficher un message de succès
+    Swal.fire({
+        title:'Succès !',
+        text:'Inscription réussie !',
+        icon:'success',
+        confirmButtonText:'OK'
+    }).then(()=>{
+
+    // Rediriger vers chat
+    router.visit('/chat');
+    });
+  } catch (error) {
+
+  console.error('Erreur complète:', error);
+  console.error('Response:', error.response);
+  console.error('Response data:', error.response?.data);
+
+  // extraction message erreur
+      let errorMessage = "Une erreur est survenue lors de l'inscription.";
+
+
+    // Afficher un pop-up d'erreur propre
+    if (error.response?.data?.errors) {
+      // Erreurs de validation (email déjà pris, etc.)
+      const errors = error.response.data.errors;
+        const firstErrorKey = Object.keys(errors)[0];
+        errorMessage = errors[firstErrorKey][0] || "Erreur de validation";
+
+    }
+    else if (errorMessage.includes("password")) {
+            errorMessage = "Le mot de passe ne respecte pas les critères requis.";
+        }
+
+
+      Swal.fire({
+  title: 'Erreur',
+  text: errorMessage,
+  icon: 'error',
+  confirmButtonText: 'OK'
+});
+  } finally {
+    chargement.value = false;
+  }
+}
+</script>
