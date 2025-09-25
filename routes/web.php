3e210aaed931e276fc2ca500968ad59030b2aa45
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2025-09-25 15:33:18 +0200

    ajout de quelque routes

--- routes/web.php
+++ routes/web.php
@@ -4,30 +4,43 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use Illuminate\Support\Facades\Log;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::post('/register', [RegisteredUserController::class, 'register']);
+
+// Routes explicites pour login et register
+Route::post('/login', [RegisteredUserController::class, 'login'])->name('login');
+
+Route::get('/register', function () {
+    return Inertia::render('Authpage');
+})->name('register');
+
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/chat', function (){
+    return Inertia::render('Chat');
+})->middleware(['auth'])->name('chat');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/login', fn () => inertia('Login'))->name('login');
-Route::get('/register', fn () => inertia('Register'))->name('register');
+
 });
 Route::get('/', function () {
     return Inertia::render('Authpage');
 })->name('home');
 
 
-require __DIR__.'/auth.php';
+
+
+Route::get('/{any}', function () {
+    return Inertia::render('Authpage');
+})->where('any', '.*');
+
+
+
