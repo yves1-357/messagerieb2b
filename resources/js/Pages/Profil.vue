c9415b2a50b8644f4e73181dc58b3339dce079bb
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2025-10-27 17:24:45 +0100

    retrait du freeze sur le log/out

--- resources/js/Pages/Profil.vue
+++ resources/js/Pages/Profil.vue
@@ -829,7 +829,7 @@ const performLogout = async () => {
   isLoggingOut.value = true;
 
   try {
-    await axios.post('/api/auth/logout');
+    await axios.post('/logout');
 
     // Rediriger vers la page d'accueil
     window.location.href = '/';
